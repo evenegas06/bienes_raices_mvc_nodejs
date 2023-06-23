77574cf15816c80a5bcf7e7fb973c53a85233141
Eduardo Venegas <evenegas06@hotmail.com>
2023-06-23 01:21:04 -0600

    view register...

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
+/**
+ * 
+ * @param {*} request 
+ * @param {*} response 
+ */
 export const loginForm = (request, response) => {
     response.render('auth/login', {
         auth: true
     });
 };
 
+/**
+ * 
+ * @param {*} request 
+ * @param {*} response 
+ */
 export const registerForm = (request, response) => {
     response.render('auth/register', {
         title: "Crear cuenta"
