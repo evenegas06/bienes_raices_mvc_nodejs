28dc73a09892af8c4426d9c6f7a5b9f4f63e5f70
Eduardo Venegas <evenegas06@hotmail.com>
2024-06-16 12:28:43 -0600

    Property model... #80

--- models/Property.js
+++ models/Property.js
@@ -15,5 +15,40 @@ const Property = db.define('properties', {
     description: {
         type: DataTypes.TEXT,
         allowNull: false,
+    },
+    bedrooms: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    parking: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    bathrooms: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    street: {
+        type: DataTypes.STRING(70),
+        allowNull: false,
+    },
+    lat: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lng: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    published: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
     }
 });
+
+export default Property;
